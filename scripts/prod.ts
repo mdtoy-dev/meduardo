603f12ebd84964299293e022d50e3cd7855168d3
mdtoy-dev <berk.toy1@gmail.com>
2024-04-04 10:07:09 +0100

    updated

--- scripts/prod.ts
+++ scripts/prod.ts
@@ -1,169 +1,169 @@
-import "dotenv/config"
-import { drizzle } from "drizzle-orm/neon-http"
-import { neon } from "@neondatabase/serverless"
+// import "dotenv/config"
+// import { drizzle } from "drizzle-orm/neon-http"
+// import { neon } from "@neondatabase/serverless"
 
-import * as schema from "../db/schema"
+// import * as schema from "../db/schema"
 
-const sql = neon(process.env.DATABASE_URL!)
-const db = drizzle(sql, { schema })
+// const sql = neon(process.env.DATABASE_URL!)
+// const db = drizzle(sql, { schema })
 
-const main = async () => {
-  try {
-    console.log("Seeding database")
+// const main = async () => {
+//   try {
+//     console.log("Seeding database")
 
-    // Delete all existing data
-    await Promise.all([
-      db.delete(schema.userProgress),
-      db.delete(schema.challenges),
-      db.delete(schema.units),
-      db.delete(schema.lessons),
-      db.delete(schema.courses),
-      db.delete(schema.challengeOptions),
-      db.delete(schema.userSubscription),
-    ])
+//     // Delete all existing data
+//     await Promise.all([
+//       db.delete(schema.userProgress),
+//       db.delete(schema.challenges),
+//       db.delete(schema.units),
+//       db.delete(schema.lessons),
+//       db.delete(schema.courses),
+//       db.delete(schema.challengeOptions),
+//       db.delete(schema.userSubscription),
+//     ])
 
-    // Insert courses
-    const courses = await db
-      .insert(schema.courses)
-      .values([{ title: "Biochemistry", imageSrc: "/biochemistry.jpg" }])
-      .returning()
+//     // Insert courses
+//     const courses = await db
+//       .insert(schema.courses)
+//       .values([{ title: "Biochemistry", imageSrc: "/biochemistry.jpg" }])
+//       .returning()
 
-    // For each course, insert units
-    for (const course of courses) {
-      const units = await db
-        .insert(schema.units)
-        .values([
-          {
-            courseId: course.id,
-            title: "Unit 1",
-            description: "Biomolecules",
-            order: 1,
-          },
-          {
-            courseId: course.id,
-            title: "Unit 2",
-            description: "Cells",
-            order: 2,
-          },
-        ])
-        .returning()
+//     // For each course, insert units
+//     for (const course of courses) {
+//       const units = await db
+//         .insert(schema.units)
+//         .values([
+//           {
+//             courseId: course.id,
+//             title: "Unit 1",
+//             description: "Biomolecules",
+//             order: 1,
+//           },
+//           {
+//             courseId: course.id,
+//             title: "Unit 2",
+//             description: "Cells",
+//             order: 2,
+//           },
+//         ])
+//         .returning()
 
-      // For each unit, insert lessons
-      for (const unit of units) {
-        const lessons = await db
-          .insert(schema.lessons)
-          .values([
-            { unitId: unit.id, title: "Amino acids and proteins", order: 1 },
-            {
-              unitId: unit.id,
-              title: "Enzyme structure and function",
-              order: 2,
-            },
-            { unitId: unit.id, title: "Enzyme kinetics", order: 3 },
-            { unitId: unit.id, title: "DNA", order: 4 },
-            { unitId: unit.id, title: "Genetics", order: 5 },
-          ])
-          .returning()
+//       // For each unit, insert lessons
+//       for (const unit of units) {
+//         const lessons = await db
+//           .insert(schema.lessons)
+//           .values([
+//             { unitId: unit.id, title: "Amino acids and proteins", order: 1 },
+//             {
+//               unitId: unit.id,
+//               title: "Enzyme structure and function",
+//               order: 2,
+//             },
+//             { unitId: unit.id, title: "Enzyme kinetics", order: 3 },
+//             { unitId: unit.id, title: "DNA", order: 4 },
+//             { unitId: unit.id, title: "Genetics", order: 5 },
+//           ])
+//           .returning()
 
-        // For each lesson, insert challenges
-        for (const lesson of lessons) {
-          const challenges = await db
-            .insert(schema.challenges)
-            .values([
-              {
-                lessonId: lesson.id,
-                type: "SELECT",
-                question:
-                  'A polypeptide with a net positive charge at physiologic pH (~7.4) most likely contains amino acids with R groups of what type?"?',
-                order: 1,
-              },
-              {
-                lessonId: lesson.id,
-                type: "SELECT",
-                question:
-                  'The unique cyclic structure of which of the following amino acids plays a central role in the formation of alpha helices and beta sheets?"?',
-                order: 2,
-              },
-              {
-                lessonId: lesson.id,
-                type: "SELECT",
-                question:
-                  'The alpha helix is an example of which of the following structural properties of proteins?"?',
-                order: 3,
-              },
-            ])
-            .returning()
+//         // For each lesson, insert challenges
+//         for (const lesson of lessons) {
+//           const challenges = await db
+//             .insert(schema.challenges)
+//             .values([
+//               {
+//                 lessonId: lesson.id,
+//                 type: "SELECT",
+//                 question:
+//                   'A polypeptide with a net positive charge at physiologic pH (~7.4) most likely contains amino acids with R groups of what type?"?',
+//                 order: 1,
+//               },
+//               {
+//                 lessonId: lesson.id,
+//                 type: "SELECT",
+//                 question:
+//                   'The unique cyclic structure of which of the following amino acids plays a central role in the formation of alpha helices and beta sheets?"?',
+//                 order: 2,
+//               },
+//               {
+//                 lessonId: lesson.id,
+//                 type: "SELECT",
+//                 question:
+//                   'The alpha helix is an example of which of the following structural properties of proteins?"?',
+//                 order: 3,
+//               },
+//             ])
+//             .returning()
 
-          // For each challenge, insert challenge options
-          for (const challenge of challenges) {
-            if (challenge.order === 1) {
-              await db.insert(schema.challengeOptions).values([
-                {
-                  challengeId: challenge.id,
-                  correct: false,
-                  text: "Acidic R groups",
-                },
-                {
-                  challengeId: challenge.id,
-                  correct: true,
-                  text: "Basic R groups",
-                },
-                {
-                  challengeId: challenge.id,
-                  correct: false,
-                  text: "Aliphatic R groups",
-                },
-              ])
-            }
+//           // For each challenge, insert challenge options
+//           for (const challenge of challenges) {
+//             if (challenge.order === 1) {
+//               await db.insert(schema.challengeOptions).values([
+//                 {
+//                   challengeId: challenge.id,
+//                   correct: false,
+//                   text: "Acidic R groups",
+//                 },
+//                 {
+//                   challengeId: challenge.id,
+//                   correct: true,
+//                   text: "Basic R groups",
+//                 },
+//                 {
+//                   challengeId: challenge.id,
+//                   correct: false,
+//                   text: "Aliphatic R groups",
+//                 },
+//               ])
+//             }
 
-            if (challenge.order === 2) {
-              await db.insert(schema.challengeOptions).values([
-                {
-                  challengeId: challenge.id,
-                  correct: false,
-                  text: "Arginine",
-                },
-                {
-                  challengeId: challenge.id,
-                  correct: true,
-                  text: "Proline",
-                },
-                {
-                  challengeId: challenge.id,
-                  correct: false,
-                  text: "Lysine",
-                },
-              ])
-            }
+//             if (challenge.order === 2) {
+//               await db.insert(schema.challengeOptions).values([
+//                 {
+//                   challengeId: challenge.id,
+//                   correct: false,
+//                   text: "Arginine",
+//                 },
+//                 {
+//                   challengeId: challenge.id,
+//                   correct: true,
+//                   text: "Proline",
+//                 },
+//                 {
+//                   challengeId: challenge.id,
+//                   correct: false,
+//                   text: "Lysine",
+//                 },
+//               ])
+//             }
 
-            if (challenge.order === 3) {
-              await db.insert(schema.challengeOptions).values([
-                {
-                  challengeId: challenge.id,
-                  correct: false,
-                  text: "tertiary structure",
-                },
-                {
-                  challengeId: challenge.id,
-                  correct: false,
-                  text: "primary structure",
-                },
-                {
-                  challengeId: challenge.id,
-                  correct: true,
-                  text: "secondary structure",
-                },
-              ])
-            }
-          }
-        }
-      }
-    }
-    console.log("Database seeded successfully")
-  } catch (error) {
-    console.error(error)
-    throw new Error("Failed to seed database")
-  }
-}
+//             if (challenge.order === 3) {
+//               await db.insert(schema.challengeOptions).values([
+//                 {
+//                   challengeId: challenge.id,
+//                   correct: false,
+//                   text: "tertiary structure",
+//                 },
+//                 {
+//                   challengeId: challenge.id,
+//                   correct: false,
+//                   text: "primary structure",
+//                 },
+//                 {
+//                   challengeId: challenge.id,
+//                   correct: true,
+//                   text: "secondary structure",
+//                 },
+//               ])
+//             }
+//           }
+//         }
+//       }
+//     }
+//     console.log("Database seeded successfully")
+//   } catch (error) {
+//     console.error(error)
+//     throw new Error("Failed to seed database")
+//   }
+// }
 
-main()
+// main()
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,181 +1,181 @@
-import "dotenv/config"
-import { neon } from "@neondatabase/serverless"
-import { drizzle } from "drizzle-orm/neon-http"
+// import "dotenv/config"
+// import { neon } from "@neondatabase/serverless"
+// import { drizzle } from "drizzle-orm/neon-http"
 
-import * as schema from "../db/schema"
+// import * as schema from "../db/schema"
 
-const sql = neon(process.env.DATABASE_URL!)
-const db = drizzle(sql, { schema })
+// const sql = neon(process.env.DATABASE_URL!)
+// const db = drizzle(sql, { schema })
 
-const main = async () => {
-  try {
-    console.log("Seeding database")
+// const main = async () => {
+//   try {
+//     console.log("Seeding database")
 
-    await db.delete(schema.courses)
-    await db.delete(schema.userProgress)
-    await db.delete(schema.units)
-    await db.delete(schema.lessons)
-    await db.delete(schema.challenges)
-    await db.delete(schema.challengeOptions)
-    await db.delete(schema.challengeProgress)
-    await db.delete(schema.userSubscription)
+//     await db.delete(schema.courses)
+//     await db.delete(schema.userProgress)
+//     await db.delete(schema.units)
+//     await db.delete(schema.lessons)
+//     await db.delete(schema.challenges)
+//     await db.delete(schema.challengeOptions)
+//     await db.delete(schema.challengeProgress)
+//     await db.delete(schema.userSubscription)
 
-    await db.insert(schema.courses).values([
-      {
-        id: 1,
-        title: "Cell Biology",
-        imageSrc: "/cell-biology.jpg",
-      },
-      {
-        id: 2,
-        title: "Biochemistry",
-        imageSrc: "/biochemistry.jpg",
-      },
-      {
-        id: 3,
-        title: "Microbiology",
-        imageSrc: "/microbiology.jpg",
-      },
-      {
-        id: 4,
-        title: "Physiology",
-        imageSrc: "/physiology.jpg",
-      },
-      {
-        id: 5,
-        title: "Pharmacology",
-        imageSrc: "/pharmacology.jpg",
-      },
-      {
-        id: 6,
-        title: "Anatomy",
-        imageSrc: "/anatomy.jpg",
-      },
-      {
-        id: 7,
-        title: "Immunology",
-        imageSrc: "/immunology.jpg",
-      },
-    ])
+//     await db.insert(schema.courses).values([
+//       {
+//         id: 1,
+//         title: "Cell Biology",
+//         imageSrc: "/cell-biology.jpg",
+//       },
+//       {
+//         id: 2,
+//         title: "Biochemistry",
+//         imageSrc: "/biochemistry.jpg",
+//       },
+//       {
+//         id: 3,
+//         title: "Microbiology",
+//         imageSrc: "/microbiology.jpg",
+//       },
+//       {
+//         id: 4,
+//         title: "Physiology",
+//         imageSrc: "/physiology.jpg",
+//       },
+//       {
+//         id: 5,
+//         title: "Pharmacology",
+//         imageSrc: "/pharmacology.jpg",
+//       },
+//       {
+//         id: 6,
+//         title: "Anatomy",
+//         imageSrc: "/anatomy.jpg",
+//       },
+//       {
+//         id: 7,
+//         title: "Immunology",
+//         imageSrc: "/immunology.jpg",
+//       },
+//     ])
 
-    await db.insert(schema.units).values([
-      {
-        id: 1,
-        courseId: 1,
-        title: "Unit 1",
-        description: "Learn the basics of Cell Biology",
-        order: 1,
-      },
-    ])
+//     await db.insert(schema.units).values([
+//       {
+//         id: 1,
+//         courseId: 1,
+//         title: "Unit 1",
+//         description: "Learn the basics of Cell Biology",
+//         order: 1,
+//       },
+//     ])
 
-    await db.insert(schema.lessons).values([
-      {
-        id: 1,
-        unitId: 1,
-        order: 1,
-        title: "Introduction to cell",
-      },
-      {
-        id: 2,
-        unitId: 1,
-        order: 2,
-        title: "Eukaryotic cells",
-      },
-      {
-        id: 3,
-        unitId: 1,
-        order: 3,
-        title: "Endoplasmic reticulum and Golgi bodies",
-      },
-      {
-        id: 4,
-        unitId: 1,
-        order: 4,
-        title: "Mitochondria and Chloroplast",
-      },
-      {
-        id: 5,
-        unitId: 1,
-        order: 5,
-        title: "Cell structure and functions",
-      },
-    ])
+//     await db.insert(schema.lessons).values([
+//       {
+//         id: 1,
+//         unitId: 1,
+//         order: 1,
+//         title: "Introduction to cell",
+//       },
+//       {
+//         id: 2,
+//         unitId: 1,
+//         order: 2,
+//         title: "Eukaryotic cells",
+//       },
+//       {
+//         id: 3,
+//         unitId: 1,
+//         order: 3,
+//         title: "Endoplasmic reticulum and Golgi bodies",
+//       },
+//       {
+//         id: 4,
+//         unitId: 1,
+//         order: 4,
+//         title: "Mitochondria and Chloroplast",
+//       },
+//       {
+//         id: 5,
+//         unitId: 1,
+//         order: 5,
+//         title: "Cell structure and functions",
+//       },
+//     ])
 
-    await db.insert(schema.challenges).values([
-      {
-        id: 1,
-        lessonId: 1,
-        type: "SELECT",
-        order: 1,
-        question: "What is the most basic unit of life?",
-      },
-      {
-        id: 2,
-        lessonId: 1,
-        type: "ASSIST",
-        order: 2,
-        question: "Ribosomes produces -> ?",
-      },
-    ])
-    await db.insert(schema.challenges).values([
-      {
-        id: 3,
-        lessonId: 2,
-        type: "SELECT",
-        order: 1,
-        question: "What is the most basic unit of life?",
-      },
-      {
-        id: 4,
-        lessonId: 2,
-        type: "ASSIST",
-        order: 2,
-        question: "Ribosomes produces -> ?",
-      },
-    ])
+//     await db.insert(schema.challenges).values([
+//       {
+//         id: 1,
+//         lessonId: 1,
+//         type: "SELECT",
+//         order: 1,
+//         question: "What is the most basic unit of life?",
+//       },
+//       {
+//         id: 2,
+//         lessonId: 1,
+//         type: "ASSIST",
+//         order: 2,
+//         question: "Ribosomes produces -> ?",
+//       },
+//     ])
+//     await db.insert(schema.challenges).values([
+//       {
+//         id: 3,
+//         lessonId: 2,
+//         type: "SELECT",
+//         order: 1,
+//         question: "What is the most basic unit of life?",
+//       },
+//       {
+//         id: 4,
+//         lessonId: 2,
+//         type: "ASSIST",
+//         order: 2,
+//         question: "Ribosomes produces -> ?",
+//       },
+//     ])
 
-    await db.insert(schema.challengeOptions).values([
-      {
-        challengeId: 1,
-        imageSrc: "/cell-biology.jpg",
-        correct: true,
-        text: "Cell",
-      },
-      {
-        challengeId: 1,
-        imageSrc: "/dna.jpg",
-        correct: false,
-        text: "DNA",
-      },
-      {
-        challengeId: 1,
-        imageSrc: "/protein.jpg",
-        correct: false,
-        text: "Protein",
-      },
-    ])
+//     await db.insert(schema.challengeOptions).values([
+//       {
+//         challengeId: 1,
+//         imageSrc: "/cell-biology.jpg",
+//         correct: true,
+//         text: "Cell",
+//       },
+//       {
+//         challengeId: 1,
+//         imageSrc: "/dna.jpg",
+//         correct: false,
+//         text: "DNA",
+//       },
+//       {
+//         challengeId: 1,
+//         imageSrc: "/protein.jpg",
+//         correct: false,
+//         text: "Protein",
+//       },
+//     ])
 
-    await db.insert(schema.challengeOptions).values([
-      {
-        challengeId: 2,
-        correct: false,
-        text: "Cell membrane",
-      },
-      {
-        challengeId: 2,
-        correct: false,
-        text: "RNA",
-      },
-      {
-        challengeId: 2,
-        correct: true,
-        text: "Protein",
-      },
-    ])
-  } catch (error) {
-    console.log(error)
-    throw new Error("Failed to seed the database")
-  }
-}
+//     await db.insert(schema.challengeOptions).values([
+//       {
+//         challengeId: 2,
+//         correct: false,
+//         text: "Cell membrane",
+//       },
+//       {
+//         challengeId: 2,
+//         correct: false,
+//         text: "RNA",
+//       },
+//       {
+//         challengeId: 2,
+//         correct: true,
+//         text: "Protein",
+//       },
+//     ])
+//   } catch (error) {
+//     console.log(error)
+//     throw new Error("Failed to seed the database")
+//   }
+// }
 
-main()
+// main()
